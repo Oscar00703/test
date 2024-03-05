95ff0898f0f469f2a5cec38c0c0139591af6d6c3
Oscar Alexander M. Abarca <10851@cofasa.com>
2024-03-05 08:28:30 -0600

    test2

--- cypress/e2e/orangeTest.cy.js
+++ cypress/e2e/orangeTest.cy.js
@@ -23,13 +23,15 @@ describe("Orange test E2E", ()=>
         cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
     })
 
-    it('validate new user', ()=>{
+    it.only('validate new user', ()=>{
         cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
         cy.get('.oxd-button').click()
         cy.get(':nth-child(1) > .oxd-main-menu-item').click()
         cy.get('.orangehrm-header-container > .oxd-button').click()
-        // cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
-
+        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input').click()
+        // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input').click()
     })
 })
